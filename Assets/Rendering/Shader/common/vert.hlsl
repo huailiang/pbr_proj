65aeff72df462c722e1d46e2a4300e13b7f269ac
penghuailiang <penghuailiang@126.com>
2018-07-31 09:45:23 +0800

    fix compile error

--- Assets/Rendering/Shader/common/vert.hlsl
+++ Assets/Rendering/Shader/common/vert.hlsl
@@ -12,24 +12,24 @@
 #include "stdlib.hlsl"
 
 
-void vertPBRForwardBase (VertexPBRInput input,out VertexPBROutput output) {
-    INITIALIZE_OUTPUT(VertexPBROutput,output);
-    output.uv0 = input.texcoord0;
+void vertPBRForwardBase (VertexPBRInput v,out VertexPBROutput o) {
+    INITIALIZE_OUTPUT(VertexPBROutput,o);
+    o.uv0 = v.texcoord0;
 
-    output.normalDir = UnityObjectToWorldNormal(input.normal);
-    output.tangentDir = normalize(mul(unity_ObjectToWorld, float4( input.tangent.xyz, 0.0)).xyz);
-    output.bitangentDir = normalize(cross(output.normalDir, output.tangentDir) * input.tangent.w);
-    output.posWorld = mul(unity_ObjectToWorld, input.vertex);
+    o.normalDir = UnityObjectToWorldNormal(v.normal);
+    o.tangentDir = normalize(mul(unity_ObjectToWorld, float4( v.tangent.xyz, 0.0)).xyz);
+    o.bitangentDir = normalize(cross(o.normalDir, o.tangentDir) * v.tangent.w);
+    o.posWorld = mul(unity_ObjectToWorld, v.vertex);
     float3 lightColor = _LightColor0.rgb;
-    output.pos = UnityObjectToClipPos(input.vertex);
-    UNITY_TRANSFER_FOG(output,output.pos);
-    TRANSFER_VERTEX_TO_FRAGMENT(output)
+    o.pos = UnityObjectToClipPos(v.vertex);
+    UNITY_TRANSFER_FOG(o,o.pos);
+    TRANSFER_VERTEX_TO_FRAGMENT(o);
 
     #ifdef LIGHTMAP_ON
-        output.ambientOrLightmapUV.xy = input.texcoord1.xy * unity_LightmapST.xy + unity_LightmapST.zw;
-        output.ambientOrLightmapUV.zw = 0;
+        o.ambientOrLightmapUV.xy = v.texcoord1.xy * unity_LightmapST.xy + unity_LightmapST.zw;
+        o.ambientOrLightmapUV.zw = 0;
     #elif UNITY_SHOULD_SAMPLE_SH
-        output.ambientOrLightmapUV = 0;
+        o.ambientOrLightmapUV = 0;
         #ifdef VERTEXLIGHT_ON
             o.ambientOrLightmapUV += Shade4PointLights (
               unity_4LightPosX0, 
@@ -40,12 +40,12 @@ void vertPBRForwardBase (VertexPBRInput input,out VertexPBROutput output) {
               unity_LightColor[2].rgb, 
               unity_LightColor[3].rgb,
               unity_4LightAtten0, 
-              output.posWorld, 
-              output.normalDir );
+              o.posWorld, 
+              o.normalDir );
         #endif // VERTEXLIGHT_ON
     #endif
     #ifdef DYNAMICLIGHTMAP_ON
-        output.ambientOrLightmapUV.zw = input.texcoord2.xy * unity_DynamicLightmapST.xy + unity_DynamicLightmapST.zw;
+        o.ambientOrLightmapUV.zw = v.texcoord2.xy * unity_DynamicLightmapST.xy + unity_DynamicLightmapST.zw;
     #endif
 }
 
